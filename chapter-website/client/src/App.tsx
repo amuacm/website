b5bfda6fdb93732c5cc54d3fe03f3eac95ea29b7
Christopher Baier <baier.tech@outlook.com>
2025-01-22 22:39:40 -0500

    cors support for dev/prod

--- chapter-website/client/src/App.tsx
+++ chapter-website/client/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   const [array, setArray] = useState([]);
 
   const fetchAPI = async () => {
-    const response = await axios.get("http://localhost:8081/api")
+    const response = await axios.get(`${process.env.REACT_APP_API_URL}`);
     setArray(response.data.fruits);
     console.log(response.data.fruits);
   }
--- chapter-website/server/server.js
+++ chapter-website/server/server.js
@@ -5,9 +5,9 @@ const path = require('path');
 const PORT = process.env.PORT || 8081;
 
 const cors = require("cors");
-const corsOptions = {
-  origin: ["http://localhost:5173"],
-}
+const corsOptions = process.env.NODE_ENV === 'production'
+  ? ['https://avemaria.acm.org']  // Production domain
+  : ['http://localhost:5173'];  // Vite dev server
 
 app.use(cors(corsOptions));
 app.use(express.static(path.join(__dirname, '../client/dist')));
